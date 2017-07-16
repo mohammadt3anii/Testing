ebebd3306d104bb2dc5f218445e3ab723be2d201
rvnmrqz <rvnmrqz@gmail.com>
2017-07-16 16:01:00 +0800

    Notification Improvements

--- app/src/main/java/com/example/rvnmrqz/firetrack/Fragment_online_reporting.java
+++ app/src/main/java/com/example/rvnmrqz/firetrack/Fragment_online_reporting.java
@@ -106,7 +106,7 @@ public class Fragment_online_reporting extends Fragment {
         txtLocation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               // showLocationChoices();
+               showLocationChoices();
             }
         });
         showLocationChoices();
