ca33cc1077611baef95c6aa84e4cbb7a12be4dc7
rvnmrqz <rvnmrqz@gmail.com>
2017-07-28 15:15:16 +0800

    Initial Development of "My Reports" module

--- app/src/main/java/com/example/rvnmrqz/firetrack/Fragment_myreports.java
+++ app/src/main/java/com/example/rvnmrqz/firetrack/Fragment_myreports.java
@@ -1,16 +1,22 @@
 package com.example.rvnmrqz.firetrack;
 
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.LinearLayout;
 
 
 public class Fragment_myreports extends Fragment {
+
+    LinearLayout layout_progress,layout_error_message,layout_list;
+
 
     public Fragment_myreports() {
         // Required empty public constructor
@@ -29,7 +35,19 @@ public class Fragment_myreports extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if(isNetworkAvailable()){
 
+        }else{
+            //show snackbar
+
+        }
+
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
     }
 
 
